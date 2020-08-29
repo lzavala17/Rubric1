b8c5c75a7febae7048e424029fd7d1b41b4f4b65
lzavala17 <56896496+lzavala17@users.noreply.github.com>
2020-08-29 00:29:29 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var x = 63;
+var y = 15;
+var z = x + y;
+	
+	if (z == 78 | z < 100) {
+		alert ("Yay! It's less than 100 degrees out!")
+	}
+	
+	var date = new Date()
+	alert("Today is " + date)  
+	
+	alert("Welcome! Please fill in your information requested and have a good day!")// JavaScript Document
